fb1e600808cb50801423b579c916c47942aa1db1
A-res <a.ress@live.ru>
2020-02-27 00:40:58 +0300

    Second half of an demo app (not final additing)

--- src/main/java/com/ares/springbootdemo/controller/UserController.java
+++ src/main/java/com/ares/springbootdemo/controller/UserController.java
@@ -2,20 +2,35 @@ package com.ares.springbootdemo.controller;
 
 import com.ares.springbootdemo.model.User;
 import com.ares.springbootdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
 public class UserController {
-
+    //почитать
+    @Autowired
     private UserService userService;
 
     @GetMapping("/users")
-    public String findAll(){
+    public String findAll(Model model){
         List<User> users = null;
+        model.addAttribute("users",users);
+        return  "user-list";
+    }
 
-        return  "";
+    @GetMapping("/user-create")
+    public String createUserForm(User user){
+        return "user-create";
+    }
+
+    //почитать
+    @PostMapping
+    public String createUser(user){
+        userService.saveUser(user);
+        return "redirect:/users";
     }
 }
